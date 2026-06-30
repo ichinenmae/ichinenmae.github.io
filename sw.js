f784c5f6bd42d8b598460ee8545b80f9f938d006
いちんねんまえ（さん） <edensugawa@gmail.com>
2026-07-01 04:23:40 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = "smdelivery-pwa-v1";
+
+const APP_SHELL = [
+  "/manifest.webmanifest",
+  "/sumureco/preview.html",
+  "/sumureco/index.html",
+  "/sumureco/smrc-icon.png",
+  "/sumureco/smrc-icon-512.png",
+  "/sumucopy/index.html",
+  "/sumucopy/templates.json",
+  "/sumucopy/title-image.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (
+          !response
+          || response.status !== 200
+          || response.type !== "basic"
+        ) {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
